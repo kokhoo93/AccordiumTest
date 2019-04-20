a07c541a8e9b2634d05c0cd117905f23d935ef8e
kokhoo <kokhoo.tan@hotmail.com>
2019-04-20 20:05:02 +0800

    test commit

--- src/main/java/com/example/demo/AccordiumTestApplication.java
+++ src/main/java/com/example/demo/AccordiumTestApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AccordiumTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccordiumTestApplication.class, args);
 	}
-
+	
+	@RequestMapping(value = "/")
+	public static String demo() {
+		return "Hello World";
+	}
 }
